5341b5675467015777e14df79d090638e022af5f
Neha Delaffon <nd4g25@soton.ac.uk>
2026-03-25 11:32:20 +0000

    re-order lines

--- analyse-coffee.py
+++ analyse-coffee.py
@@ -8,8 +8,8 @@ def normalise_points(min, max, score):
     return normal_points
 
 ### total_points() returns a score from 0-4
-def yum_score(f_points, b_points, a_points, u_points):
-    total_points = f_points + b_points + a_points + u_points
+def yum_score(a_points, f_points, b_points, u_points):
+    total_points = a_points + f_points + b_points + u_points
     return total_points
 
 def main(input_csv_file: str = "data/clean/coffee_ratings.csv", output_csv_file: str = "data/clean/coffee_ratings_yscore.csv"):
@@ -27,12 +27,12 @@ def main(input_csv_file: str = "data/clean/coffee_ratings.csv", output_csv_file:
 
     ### Getting the min and max values
     df = pd.read_csv(input_csv_file)
+    max_aroma = df["aroma"].max()
+    min_aroma = df["aroma"].min()
     max_flavour = df["flavour"].max()
     min_flavour = df["flavour"].min()
     max_body = df["body"].max()
     min_body = df["body"].min()
-    max_aroma = df["aroma"].max()
-    min_aroma = df["aroma"].min()
     max_uniformity = df["uniformity"].max()
     min_uniformity = df["uniformity"].min()
 
@@ -49,11 +49,11 @@ def main(input_csv_file: str = "data/clean/coffee_ratings.csv", output_csv_file:
 
         ### Loop through remaining rows to calculate the yum score ###
         for row in reader:
+            aroma_points = normalise_points(min_aroma, max_aroma, row['aroma'])
             flavour_points = normalise_points(min_flavour, max_flavour, row['flavour'])
             body_points = normalise_points(min_body, max_body, row['body'])
-            aroma_points = normalise_points(min_aroma, max_aroma, row['aroma'])
             uniformity_points = normalise_points(min_uniformity, max_uniformity, row['uniformity'])
-            y_score = yum_score(flavour_points, body_points, aroma_points, uniformity_points)
+            y_score = yum_score(aroma_points, flavour_points, body_points, uniformity_points)
             row.append(y_score)
             writer.writerow(row)
 
